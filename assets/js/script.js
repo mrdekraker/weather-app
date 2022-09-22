c7401394908168827c5b6e15a257dbea279d4374
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-09-22 16:59:45 -0500

    updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+console.log(dayjs());
+
 const cities = [];
 
+const date = dayjs(new Date());
+const weatherList = $(`#weather-list`);
+const weatherContainer = $(`.currentWeather`);
+const cityCurrentDate = $(`#cityCurrentDate`);
 const citySubmit = $(`#city-form`);
 const cityInput = $(`#city`);
 
+console.log(date.format('dddd, MMM D, YYYY')); // '25/01/2019'););
 // List Past Searches
 const recentSearch = function (recentSearch) {
   console.log(recentSearch);
@@ -13,20 +20,52 @@ const recentSearch = function (recentSearch) {
 
   const recentSearchEl = $(`ulEl`).append(`li`);
   $(recentSearchEl).text(recentSearch);
+  $(recentSearchEl).addClass(`.`);
 };
 
 // Display the weather on the page
 const displayWeather = function (weather, citySearch) {
   // clear old content
-  weatherContainerEl.textContent = ``;
+  $(weatherContainer).text(``);
+  $(cityCurrentDate).text(citySearch);
+
+  // Add Date Element
+  const currentDateSpan = $(`<span>`);
+  $(currentDateSpan).text(` ${date.format('(dddd, MMM D, YYYY)')}`);
+  $(cityCurrentDate).append(currentDateSpan);
+
+  // Add Weather Icon
+  const weatherIcon = $(`img`);
+  $(weatherIcon).attr(`src`, `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+  $(weatherContainer).append(weatherIcon);
+
+  // Create Li to append to weatherContainer to hold temperature data
+  const tempLi = $(`li`);
+  $(tempLi).text(`Temperature: ${weather.main.temp} °F`);
+  $(weatherList).append(tempLi);
+
+  // Create Li to hold humidity data
+  const humidLi = $(`li`);
+  $(humidLi).text(`Humidity: ${weather.main.humidity} %`);
+  $(weatherList).append(humidLi);
+
+  // Create Li to hold wind Data
+  const windLi = $(`li`);
+  $(windLi).text(`Wind Speed: ${weather.wind.speed} MPH`);
+  $(weatherList).append(windLi);
+
+  const { lat } = weather.coord;
+  const { lon } = weather.coord;
+
+  getUVIndex(lat, lon);
 };
 
 // Retrieve the forcast
 const getForcast = function (city) {
-  const apiKey = `44d25aa6fc100aefcb5aaa543a561628`;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+  const APIKey = `44d25aa6fc100aefcb5aaa543a561628`;
+  const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q={${city}}&appid={${APIKey}}`;
 
-  fetch(apiUrl).then((response) => {
+  fetch(APIUrl).then((response) => {
     response.JSON().then((data) => {
       displayWeather(data, city);
     });
@@ -44,7 +83,7 @@ const formSubmitHandler = function (event) {
   const city = $(cityInput).val().trim();
 
   if (city) {
-    getCityWeather(city);
+    getForcast(city);
     getForcast(city);
     $(cities).unshift({ city });
     $(cityInput).val(``);
@@ -57,3 +96,5 @@ const formSubmitHandler = function (event) {
 };
 
 $(citySubmit).submit(formSubmitHandler);
+
+// https://github.com/mlportu/weather-dashboard/blob/master/assets/js/script.js
